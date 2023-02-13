bff27a8974c3063ff6425627d5e75a4ea3029fe9
Nicole <nicole.polis@ga.ntig.se>
2023-02-13 12:57:48 +0100

    Update

---
+++ uppg1_InputOutput_txt/uppgift1c.py
@@ -0,0 +1,21 @@
+# Ditt namn
+# Din klass
+# Datum
+
+# Kommentarer
+# Du ska läsa in från en fil, gå igenom den rad för rad och få ut det hemliga meddelandet!
+# Vilka hjälp-funktioner behöver du definera?
+# Din kod nedan.
+
+
+
+def yourFunction():
+    pass
+
+
+def main():
+    filePath = ""
+    with open(filePath, "r",encoding="UTF8") as file:
+        for line in file:
+            pass
+main()
